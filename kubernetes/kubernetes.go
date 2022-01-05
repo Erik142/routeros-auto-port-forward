2f99f6916a90c26a72d6e803cec840124f59baf3
Erik Wahlberger <ewahlberger@gmail.com>
2022-01-06 00:12:07 +0100

    Add basic polling of services and port forwards

--- kubernetes/kubernetes.go
+++ kubernetes/kubernetes.go
@@ -2,9 +2,15 @@ package kubernetes
 
 import (
 	"context"
+	"log"
+	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+	"time"
 
+	"github.com/Erik142/routeros-auto-port-forward/mikrotik"
+	"github.com/go-routeros/routeros"
 	"github.com/pborman/getopt"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,7 +22,9 @@ import (
 const PortForwardEnabledAnnotation = "routeros.portforward.enabled"
 const PortForwardPorts = "routeros.portforward.ports"
 
-func CreateClientSet() (*kubernetes.Clientset, error) {
+var routerOsClient *routeros.Client
+
+func createClientSet() (*kubernetes.Clientset, error) {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = getopt.StringLong("kubeconfig", 'c', filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -34,7 +42,7 @@ func CreateClientSet() (*kubernetes.Clientset, error) {
 	return kubernetes.NewForConfig(config)
 }
 
-func GetServices(c *kubernetes.Clientset) ([]v1.Service, error) {
+func getServices(c *kubernetes.Clientset) ([]v1.Service, error) {
 	listOptions := metav1.ListOptions{}
 	serviceList, err := c.CoreV1().Services("").List(context.Background(), listOptions)
 
@@ -54,4 +62,123 @@ func GetServices(c *kubernetes.Clientset) ([]v1.Service, error) {
 	}
 
 	return loadBalancerServices, nil
+}
+
+func Close() {
+	routerOsClient.Close()
+}
+
+func Listen() {
+	address := getopt.StringLong("address", 'a', "localhost:8728", "The complete URL to the mikrotik router, for example: localhost:8728")
+	username := getopt.StringLong("user", 'u', "admin", "The username to login with on the MikroTik router.")
+	password := getopt.StringLong("password", 'p', "password", "The password for the router that will be used to login on the MikroTik router.")
+	getopt.Parse()
+
+	kubernetesClientSet, err := createClientSet()
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	routerOsClient, err = routeros.Dial(*address, *username, *password)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	for {
+		services, err := getServices(kubernetesClientSet)
+
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(-1)
+		}
+
+		portForwards, err := mikrotik.GetAllPortForwards(*routerOsClient)
+
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(-1)
+		}
+
+		oldPortForwards, err := getOldPortForwards(portForwards, services)
+
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(-1)
+		}
+
+		newPortForwards, err := getNewPortForwards(portForwards, services)
+
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(-1)
+		}
+
+		if oldPortForwards != nil && len(oldPortForwards) > 0 {
+			for _, portForward := range oldPortForwards {
+				log.Printf("Deleting the port forward for the service %s in the namespace %s\n", portForward.Name, portForward.Namespace)
+				mikrotik.DeletePortForward(*routerOsClient, portForward)
+			}
+		}
+
+		if newPortForwards != nil && len(newPortForwards) > 0 {
+			for _, portForward := range newPortForwards {
+				log.Printf("Adding a new port forward for the service %s in the namespace %s\n", portForward.Name, portForward.Namespace)
+				mikrotik.AddPortForward(*routerOsClient, portForward)
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func getOldPortForwards(allPortForwards []mikrotik.PortForward, allServices []v1.Service) ([]mikrotik.PortForward, error) {
+	oldPortForwards := []mikrotik.PortForward{}
+
+	if len(allServices) == 0 {
+		return allPortForwards, nil
+	}
+
+	for _, portForward := range allPortForwards {
+		index := sort.Search(len(allServices), func(i int) bool {
+			return allServices[i].Namespace == portForward.Namespace && allServices[i].Status.LoadBalancer.Ingress[0].IP == portForward.DestinationIp && allServices[i].Name == portForward.Name
+		})
+
+		if index == len(allServices) {
+			oldPortForwards = append(oldPortForwards, portForward)
+		}
+	}
+
+	return oldPortForwards, nil
+}
+
+func getNewPortForwards(allPortForwards []mikrotik.PortForward, allServices []v1.Service) ([]mikrotik.PortForward, error) {
+	newPortForwards := []mikrotik.PortForward{}
+
+	for _, service := range allServices {
+		index := sort.Search(len(allPortForwards), func(i int) bool {
+			return allPortForwards[i].Namespace == service.Namespace && allPortForwards[i].DestinationIp == service.Status.LoadBalancer.Ingress[0].IP && allPortForwards[i].Name == service.Name
+		})
+
+		if index == len(allPortForwards) && len(service.Status.LoadBalancer.Ingress) > 0 && service.Status.LoadBalancer.Ingress[0].IP != "" {
+			for _, servicePort := range service.Spec.Ports {
+				destinationPort := int(servicePort.Port)
+				toPort := int(servicePort.Port)
+
+				newPortForwards = append(newPortForwards, mikrotik.PortForward{
+					Namespace:       service.Namespace,
+					Name:            service.Name,
+					DestinationPort: destinationPort,
+					DestinationIp:   service.Status.LoadBalancer.Ingress[0].IP,
+					ToPort:          toPort,
+					Protocol:        strings.ToLower(string(servicePort.Protocol)),
+				})
+			}
+		}
+	}
+
+	return newPortForwards, nil
 }
---
+++ mikrotik/mikrotik.go
@@ -0,0 +1,130 @@
+package mikrotik
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/go-routeros/routeros"
+	"github.com/go-routeros/routeros/proto"
+)
+
+const BaseAddPortForwardCommand = "/ip/firewall/nat/add|=chain=dstnat|=dst-port=%d|=action=dst-nat|=protocol=%s|=to-addresses=%s|=to-ports=%d|=comment=%s|=in-interface-list=WAN"
+const BaseDeletePortForwardCommand = "/ip/firewall/nat/remove|=.id=%s"
+
+const BaseGetPortForwardsCommand = "/ip/firewall/nat/getall|?comment=%s|=.proplist=.id,comment,dst-port,to-addresses,to-ports"
+const BaseGetPortForwardsUnequalCommand = "/ip/firewall/nat/getall|?>comment=%s|=.proplist=.id,comment,dst-port,to-addresses,to-ports"
+
+const PortForwardPrefix = "routeros.autoport"
+
+type PortForward struct {
+	Namespace       string
+	Name            string
+	DestinationPort int
+	ToPort          int
+	DestinationIp   string
+	Protocol        string
+}
+
+func GetAddPortForwardCommand(destinationIp string, destinationPort int, routerPort int, protocol string, comment string) []string {
+	return strings.Split(fmt.Sprintf(BaseAddPortForwardCommand, routerPort, protocol, destinationIp, destinationPort, comment), "|")
+}
+
+func GetDeletePortForwardCommand(comment string) []string {
+	return strings.Split(fmt.Sprintf(BaseDeletePortForwardCommand, comment), "|")
+}
+
+func GetPortForwardsCommand(comment string, equal bool) []string {
+	if equal {
+		return strings.Split(fmt.Sprintf(BaseGetPortForwardsCommand, comment), "|")
+	} else {
+		return strings.Split(fmt.Sprintf(BaseGetPortForwardsUnequalCommand, comment), "|")
+	}
+}
+
+func AddPortForward(client routeros.Client, portForward PortForward) (bool, error) {
+	portForwardCommand := GetAddPortForwardCommand(portForward.DestinationIp, portForward.DestinationPort, portForward.DestinationPort, portForward.Protocol, fmt.Sprintf("%s.%s.%s", PortForwardPrefix, portForward.Namespace, portForward.Name))
+	log.Println(portForwardCommand)
+	_, err := client.RunArgs(portForwardCommand)
+
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+
+	return true, nil
+}
+
+func DeletePortForward(client routeros.Client, portForward PortForward) (bool, error) {
+	sentences, err := getPortForwards(client, portForward)
+
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+
+	for _, sentence := range sentences {
+		id := sentence.Map[".id"]
+		deletePortForwardCommand := GetDeletePortForwardCommand(id)
+
+		_, err := client.RunArgs(deletePortForwardCommand)
+
+		if err != nil {
+			log.Fatal(err)
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+func GetAllPortForwards(client routeros.Client) ([]PortForward, error) {
+	sentences, err := getPortForwards(client, PortForward{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	portForwards := []PortForward{}
+
+	for _, sentence := range sentences {
+		comment, ok := sentence.Map["comment"]
+
+		if ok {
+			data := strings.ReplaceAll(comment, fmt.Sprintf("%s.", PortForwardPrefix), "")
+			namespace := strings.SplitN(data, ".", 2)[0]
+			name := strings.SplitN(data, ".", 2)[1]
+			destinationPort, _ := strconv.Atoi(sentence.Map["dst-port"])
+			toPort, _ := strconv.Atoi(sentence.Map["to-port"])
+
+			portForwards = append(portForwards, PortForward{
+				Namespace:       namespace,
+				Name:            name,
+				DestinationIp:   sentence.Map["to-addresses"],
+				DestinationPort: destinationPort,
+				ToPort:          toPort,
+			})
+		}
+
+	}
+	return portForwards, nil
+}
+
+func getPortForwards(client routeros.Client, portForward PortForward) ([]*proto.Sentence, error) {
+	var getPortForwardsCommand []string
+	if portForward.Name == "" && portForward.Namespace == "" {
+		getPortForwardsCommand = GetPortForwardsCommand(fmt.Sprintf("%s", PortForwardPrefix), false)
+	} else {
+		getPortForwardsCommand = GetPortForwardsCommand(fmt.Sprintf("%s.%s.%s", PortForwardPrefix, portForward.Namespace, portForward.Name), true)
+	}
+
+	reply, err := client.RunArgs(getPortForwardsCommand)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return reply.Re, nil
+}
